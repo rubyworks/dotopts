0aac803e303f0be8f908c60bbd63c556b0f77ba4
7rans <transfire@gmail.com>
2013-02-15 12:19:01 -0500

    Add spec for user added arguments. [test]

--- spec/spec_api.rb
+++ spec/spec_api.rb
@@ -3,8 +3,8 @@ require_relative "helper"
 describe DotOpts do
 
   it "can configure!" do
-    ARGV.replace([])
     ENV['cmd'] = 'yard'
+    ARGV.replace([])
 
     example_avex_file = File.dirname(__FILE__) + '/fixtures/yard.opts'
 
@@ -12,6 +12,17 @@ describe DotOpts do
 
     ARGV[-2].assert == '--title'
     ARGV[-1].assert == 'Big Title'
+  end
+
+  it "can should not configure! when arguments are given" do
+    ENV['cmd'] = 'yard'
+    ARGV.replace(['--private'])
+
+    example_avex_file = File.dirname(__FILE__) + '/fixtures/yard.opts'
+
+    DotOpts.configure!(example_avex_file)
+
+    ARGV[-1].assert == '--private'
   end
 
 end
